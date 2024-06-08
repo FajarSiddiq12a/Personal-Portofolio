cc79ed22267e1a39663e966b5884a86167f0abaf
FajarSiddiq12a <fajarsetiawans167@gmail.com>
2024-06-08 13:23:30 +0700

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src**/*.{vue,js,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors :{
+        'background' : '#29241e',
+        'text': '#f7f9fe',
+        'title': '#4cb6f7',
+        'title-hover': '#5ca8ff', 
+        'bg' : '#1d2120',
+      }
+    },
   },
   plugins: [],
 }
